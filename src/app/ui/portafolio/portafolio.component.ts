3eba2c88d6be0a6a8111a5484e3a0accbf022c14
Daniel Alejandro Mesa Lopez <146145426+DanielMesaLopez@users.noreply.github.com>
2024-12-07 22:46:55 -0500

    cambios en sass y html

--- src/app/ui/portafolio/portafolio.component.ts
+++ src/app/ui/portafolio/portafolio.component.ts
@@ -12,14 +12,22 @@ export class PortafolioComponent implements OnInit {
   isCollapsedTres = true;
   isCollapsedCuatro = true;
 
+  // Propiedades para controlar el giro de cada tarjeta
+  flipCardOne: boolean = false;
+  flipCardTwo: boolean = false;
+  flipCardThree: boolean = false;
+  flipCardFour: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {}
 
+  // Método para alternar el estado de colapso de las secciones
   toggleSection(section: string) {
     this.activeSection = this.activeSection === section ? '' : section;
   }
 
+  // Método para alternar el colapso del contenido en las secciones
   toggleContent(section: string) {
     if (section === 'uno') {
       this.isCollapsedUno = !this.isCollapsedUno;
@@ -29,6 +37,36 @@ export class PortafolioComponent implements OnInit {
       this.isCollapsedTres = !this.isCollapsedTres;
     } else if (section === 'cuatro') {
       this.isCollapsedCuatro = !this.isCollapsedCuatro;
+    }
+  }
+
+  // Método para voltear las tarjetas
+  flipCard(cardNumber: number): void {
+    switch (cardNumber) {
+      case 1:
+        this.flipCardOne = !this.flipCardOne;
+        break;
+      case 2:
+        this.flipCardTwo = !this.flipCardTwo;
+        break;
+      case 3:
+        this.flipCardThree = !this.flipCardThree;
+        break;
+      case 4:
+        this.flipCardFour = !this.flipCardFour;
+        break;
+      default:
+        break;
+    }
+  }
+  // Método para voltear las tarjetas
+  flipCardAC(cardNumber: number): void {
+    switch (cardNumber) {
+      case 1:
+        this.flipCardOne = !this.flipCardOne;
+        break;
+      default:
+        break;
     }
   }
 }
